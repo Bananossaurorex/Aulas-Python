89f5b24e4b67dc10508241b482af1f4774c8a84e
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2024-05-02 13:34:47 +0000

    inicios python

---
+++ somente pares.py
@@ -0,0 +1,12 @@
+import os
+os.system("clear")
+print("Pares: ")
+for i in range (100,121):
+    if i %2==0:
+        print(f"- {i}")
+
+print("Impares: ")
+for j in range (100,121):
+    if j %2==1:
+        print(f"- {j}")
+
